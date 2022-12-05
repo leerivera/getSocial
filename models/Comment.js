feec3a7205e3a732874cccd14112b6a3828afa40
leerivera <leerivera80@gmail.com>
2022-12-05 14:22:08 -0500

    updated comment model

--- models/Comment.js
+++ models/Comment.js
@@ -14,6 +14,12 @@ const CommentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post",
   },
+  createdById: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+    
+
+  },
   createdAt: {
     type: Date,
     default: Date.now,
