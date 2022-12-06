b19d11de3350c19d0e86c75a9fd092aac907a457
leerivera <leerivera80@gmail.com>
2022-12-06 16:06:51 -0500

    updated comment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,5 +1,5 @@
 const Comment = require("../models/Comment");
-const User = require("../models/User");
+const { getAllCommentsIterative } = require("../middleware/comment")
 
 module.exports = {
   createComment: async (req, res) => {
@@ -7,11 +7,15 @@ module.exports = {
         //User is part of req so we dont have to deconstruct an object
       const commentUser = await User.findById(req.user.id)
       await Comment.create({
-        comment: req.body.comment,
+        // text user post comment deleted edited to object
+        text: req.body.text,
+        user: req.user.id,
+        post: req.params.commentId ? udenfined: req.params.postid,
+        comment: req.body.commentId,
         likes: 0,
-        post: req.params.id,
-        createdBy: commentUser.userName,
-        createdById: req.user.id
+        // post: req.params.id,
+        // createdBy: commentUser.userName,
+        // createdById: req.user.id
       });
       console.log("Comment has been added!");
       res.redirect("/post/"+req.params.id);
@@ -22,11 +26,35 @@ module.exports = {
   //! Added delete comment method
   deleteComment: async (req, res) => {
     try {
-      await Comment.deleteOne({ _id: req.params.commentid })
-      console.log("comment removed")
-      res.redirect("/post/"+req.params.postid);
-    } catch (err) {
-      console.log(err);
+    const comment = await getAllCommentsIterative(...await Comment.findById(req.params.commentId))[0];
+    if(!comment.comments.length){
+        await comment.remove()
+        console.log("comment deleted");
+        return res.redirect("/post/" + req.params.postid);
+    }
+    comment.text = '';
+    comment.deleted = true;
+    await comment.save();
+    console.log("Comment Cleared");
+    res.redirect("/post/" + req.params.postId);
+   
+  } catch(err){
+    console.log(err);
+   }
+  },
+
+  editComment: async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        comment.text = req.body.text;
+        comment.edited = true;
+
+        await comment.save();
+        console.log("Comment has been edited");
+        res.redirect("/post/" + req.params.postId);
+    }
+    catch(err) {
+        console.log(err);
     }
   }
 //   likePost: async (req, res) => {
