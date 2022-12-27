ac5e5fdfc8f00cad613131f79320b9bdd07db349
leerivera <leerivera80@gmail.com>
2022-12-27 09:52:45 -0500

    controller follow

---
+++ controllers/follow.js
@@ -0,0 +1,21 @@
+const Follow = require('../models/Follow');
+const User = require('../models/User');
+
+module.exports = {
+    followUser: async (req, res) => {
+        const { receiver } = req.params;
+        const sender = req.user._id;
+        const follow  = await Follow.findOne({ sender, receiver });
+        if (follow) return res.status(400).end();
+        const newFollow = await newFollow.save();
+        fullFollow.sender = req.user;
+        fullFollow.receiver = await User.findById(receiver);
+        res.json(fullFollow);
+    },
+    unfollowUser: async (req, res) => {
+        const { receiver } = req.params;
+        const sender = req.user._id;
+        const follow = await Follow.findOneAndDelete({ sender, receiver });
+        res.json(follow);
+    }
+}
